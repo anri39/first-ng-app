281abd167648eb06bdceae81b9cb04ea1b8f1324
Anri Ugrekhelidze <anriugrexelidze2006@gmail.com>
2025-10-16 22:00:39 +0400

    Angular test ( counter app )

---
+++ src/app/components/counter/counter.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+
+  templateUrl: './counter.html',
+  styleUrls: ['./counter.css'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class Counter {
+  initialIncrement = signal(0);
+
+  increment() {
+    this.initialIncrement.update((prev) => prev + 1);
+  }
+
+  decrement() {
+    this.initialIncrement.update((prev) => prev - 1);
+  }
+
+  reset() {
+    this.initialIncrement.set(0);
+  }
+}
